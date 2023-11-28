fca73096893acb33855571226758de679670a40a
Elina <elinanurgal@gmail.com>
2023-11-28 21:44:51 +0300

    added board, paddlet, ball graphics

---
+++ semester_work-2/src/main/java/com/example/semester_work2/PongApplication.java
@@ -0,0 +1,16 @@
+package com.example.semester_work2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PongApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
